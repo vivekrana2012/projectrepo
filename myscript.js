a673d63c5ccfdf1f2107d87483fdcb3ce3217474
vivekrana2012 <vivekrana.2012@gmail.com>
2017-04-18 18:43:55 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,28 @@
+﻿ 
+$("#jump").click(function(){
+                $('html,body').animate({scrollTop: $("#targetDiv").offset().top},1300);
+});
+ 
+$(".title").click(function(){
+                $('html,body').animate({scrollTop: 0},1300);
+});
+
+$("#targetDiv").scroll(function(){
+				var scrollTop = $(this).scrollTop();
+				$('.parallexBg').css('top', -((scrollTop - 1600) * 0.3) + 'px');
+});
+
+$(".expand").click(function(){
+				if (!$('.sideMenu').width()) {
+					$('.sideMenu').css('width', '100%');
+					$('.rightClose').css('visibility','visible');
+				}else{
+					$('.sideMenu').css('width', '0%');
+					$('.rightClose').css('visibility','hidden');
+				}
+});
+
+$(".rightClose").click(function(){
+					$('.sideMenu').css('width', '0%');
+					$('.rightClose').css('visibility','hidden');
+});
